6504f0b939380a1be5fc0db04e191e09044335e4
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-08-02 18:11:02 +0530

    Add files via upload

---
+++ Arrays/commonElements.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+{
+            //code here.
+    vector<int> ans;
+    unordered_map<int, int> m1;
+    unordered_map<int, int> m2;
+    unordered_map<int, int> m3;
+    for(int i = 0; i < n1; i++) m1[A[i]]++;
+    for(int i = 0; i < n2; i++) m2[B[i]]++;
+    for(int i = 0; i < n3; i++) m3[C[i]]++;
+            
+    for(int i = 0; i < n1; i++){
+        if(m1[A[i]] && m2[A[i]] && m3[A[i]]){
+            ans.push_back(A[i]);
+            m1[A[i]] = 0;
+        }
+    }
+    return ans;
+}
