d9ea20e976adc87cbb62053a369d1f94cfad0c69
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-09-12 21:59:11 +0530

    Add files via upload

---
+++ Linked List/pairSum.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
+    node(int d){
+        this -> data = d;
+        this -> next = NULL;
+        this -> prev = NULL;
+    }
+};
+
+
+void pairSum(node* head, int x){
+    node* start = head;
+    node* end = head;
+    while(end -> next != NULL)
+        end = end -> next;
+    
+    bool f = false;
+
+    while(start != end && end -> next != start){
+        if((start -> data) + (end -> data) == x){
+            f = true;
+            cout << '(' << start -> data << ", " << end -> data << ')' << endl;
+        }
+        else{
+            if((start -> data) + (end -> data) < x)
+                start = start -> next;
+            else if((start -> data) + (end -> data) > x)
+                end = end -> prev;
+        }
+    }
+    if(f == false) cout << "No pairs exist" << endl;
+}
