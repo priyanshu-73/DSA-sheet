6de9c1c55354e454bd0a041573e6f386af7ba2c2
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-09-29 13:08:01 +0530

    Add files via upload

---
+++ Stack and queues/reverseUsingStack.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+char* reverse(char *S, int len)
+{
+    stack<char> s;
+    for(int i = 0; i < len; i++){
+        s.push(S[i]);
+    }
+    
+    for(int i = 0; i < len; i++){
+        S[i] = s.top();
+        s.pop();
+    }
+    return S;
+}
