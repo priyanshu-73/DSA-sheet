dfc4a8c4fa0c10b53c6785b965248fee804a7406
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-10-04 08:56:47 +0530

    Add files via upload

---
+++ Stack and queues/stackByQueue.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+void QueueStack :: push(int x)
+{
+        q1.push(x);
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+    if(q1.empty())
+        return -1;
+        
+    while(q1.size() != 1){
+        q2.push(q1.front());
+        q1.pop();
+    }
+    
+    int c = q1.front();
+    q1.pop();
+    
+    while(!q2.empty()){
+        q1.push(q2.front());
+        q2.pop();
+    }
+    return c;
+}
