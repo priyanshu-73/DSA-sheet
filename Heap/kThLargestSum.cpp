a5fb9a55b5994f2a0e5d7344a4e2d5e4187b7218
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-10-07 08:59:22 +0530

    Add files via upload

---
+++ Heap/kThLargestSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+    int kthLargest(vector<int> &arr,int n,int k){
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += arr[j];
+                pq.push(sum);
+                if(pq.size() > k)
+                    pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
