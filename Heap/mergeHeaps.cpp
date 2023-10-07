4b63e3e854516ef9a2743d55235961b01d63aac7
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-10-07 08:23:51 +0530

    Add files via upload

---
+++ Heap/mergeHeaps.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        priority_queue<int> pq;
+        for(int i = 0; i < n; i++){
+            pq.push(a[i]);
+        }
+        
+        for(int i = 0; i < m; i++){
+            pq.push(b[i]);
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
